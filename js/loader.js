18f44b5ef7dac23d4243c7e5308a037033210c7b
burmeai.io <athuyrain@gmail.com>
2025-08-05 02:39:11 +0630

    Delete js/loader.js

--- js/loader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Simulate loading process
-    setTimeout(function() {
-        window.location.href = 'src/main.html';
-    }, 3000); // Redirect after 3 seconds
-});
