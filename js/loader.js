d61d4ae936411d799fabdc83d1d51665fba0ddc2
burmeai.io <athuyrain@gmail.com>
2025-08-04 22:52:49 +0630

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Simulate loading process
+    setTimeout(function() {
+        window.location.href = 'src/main.html';
+    }, 3000); // Redirect after 3 seconds
+});
